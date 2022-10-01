b4852ec13b3038fbb227b38776a7b053f1772599
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-10-01 14:28:21 +0900

    edited

--- app/public/wp-content/plugins/gutenpride/build/index.asset.php
+++ app/public/wp-content/plugins/gutenpride/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '72105dfd1a2c9f214a12');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd36e246998b64b0892c1');
